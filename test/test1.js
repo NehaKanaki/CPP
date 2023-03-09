fc0bba3c0fd52c543d0e8ee7098e4c8eb094e2b3
NehaKanaki <nehakanaki2004@gmail.com>
2023-03-09 14:55:24 +0530

    first commit

--- test/test1.js
+++
@@ -1,9 +0,0 @@
-let submitBtn=document.getElementById('btn');
-submitBtn.addEventListener('click',display);
-
-function display(){
-    var input = document.getElementById("d1").value;
-    var input1 = document.getElementById("t1").value;
-    // alert(input+" "+ input1);
-    document.write( input+""+input1);
-}
